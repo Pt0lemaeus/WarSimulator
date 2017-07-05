ecbbab80dd821eb63ba955fb0994739dcfb9d3af
Pt0lemaeus <tuxiaomi@gmail.com>
2017-07-05 23:13:34 +0800

    add some to the Trig library

--- WarSimulator/GaiaEngine/Core/Trig.h
+++ WarSimulator/GaiaEngine/Core/Trig.h
@@ -1,4 +1,9 @@
 #pragma once
-namespace GaiaEngine_Core{
+#include <windows.h> // for XMVerifyCPUSupport
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include "../../../../Common/MathHelper.h"
 
+namespace GaiaEngine_Core{
+    const double OneOverPi = 1.0 / MathHelper::Pi;
 }
