649435678515aac9cd94e9cfb8c1aadef36b6533
Pt0lemaeus <tuxiaomi@gmail.com>
2017-07-06 00:21:53 +0800

    Add Pi relatives

--- WarSimulator/GaiaEngine/Core/Trig.h
+++ WarSimulator/GaiaEngine/Core/Trig.h
@@ -6,4 +6,12 @@
 
 namespace GaiaEngine_Core{
     const double OneOverPi = 1.0 / MathHelper::Pi;
+    const double PiOverTwo = MathHelper::Pi * 0.5;
+    const double PiOverThree = MathHelper::Pi / 3.0;
+    const double PiOverFour = MathHelper::Pi / 4.0;
+    const double PiOverSix = MathHelper::Pi / 6.0;
+    const double ThreePiOver2 = (3.0 * MathHelper::Pi) * 0.5;
+    const double TwoPi = 2.0 * MathHelper::Pi;
+    const double OneOverTwoPi = 1.0 / (2.0 * MathHelper::Pi);
+    const double RadiansPerDegree = MathHelper::Pi / 180.0;
 }
