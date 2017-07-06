c88eb1c3b3a3dcd252ccf04217d2376cdbaab883
Pt0lemaeus <tuxiaomi@gmail.com>
2017-07-06 14:14:16 +0800

    Add some files

--- WarSimulator/GaiaEngine/Core/Geodetic2D.h
+++ WarSimulator/GaiaEngine/Core/Geodetic2D.h
@@ -1,7 +1,22 @@
 #pragma once
 #include "Geodetic3D.h"
+
 namespace GaiaEngine::Core {
     class Geodetic2D {
+    public:
+        double longitude_;
+        double latitude_;
+    public:
+        Geodetic2D(double longitude, double latitude)
+        {
+            longitude_ = longitude;
+            latitude_ = latitude;
+        };
 
+        Geodetic2D(Geodetic3D geodetic3D)
+        {
+            longitude_ = geodetic3D.longitude_;
+            latitude_ = geodetic3D.longitude_;
+        };
     };
 }
--- WarSimulator/GaiaEngine/Core/Geodetic3D.h
+++ WarSimulator/GaiaEngine/Core/Geodetic3D.h
@@ -2,7 +2,38 @@
 
 namespace GaiaEngine::Core {
     class Geodetic3D {
+    public:
+        double longitude_;
+        double latitude_;
+        double height_;
+    public:
+         Geodetic3D(double longitude, double latitude, double height)
+        {
+            longitude_ = longitude;
+            latitude_ = latitude;
+            height_ = height;
+        }
 
+         Geodetic3D(double longitude, double latitude)
+        {
+            longitude_ = longitude;
+            latitude_ = latitude;
+            height_ = 0;
+        }
+
+        Geodetic3D(Geodetic2D geodetic2D)
+        {
+            longitude_ = geodetic2D.longitude_;
+            latitude_ = geodetic2D.latitude_;
+            height_ = 0;
+        }
+
+        Geodetic3D(Geodetic2D geodetic2D, double height)
+        {
+            longitude_ = geodetic2D.longitude_;
+            latitude_ = geodetic2D.latitude_;
+            height_ = height;
+        }
     };
 }
 
